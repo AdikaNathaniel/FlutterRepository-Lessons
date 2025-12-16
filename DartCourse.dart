c03b9177003857ac1213832d22835d6a261ee96f
Nathaniel Adika <nathanieladikajnr@gmail.com>
2025-12-16 06:45:38 +0000

    notes

--- DartCourse.dart
+++ DartCourse.dart
@@ -1,6 +1,11 @@
-1.Flutter is a UI framework developed by Google for building natively compiled applications for mobile, web, and desktop from a single codebase.
+1.Flutter is a UI framework developed by Google for building natively compiled applications for mobile, web, and desktop from a single codebase 
+thus building apps for multiple platforms without having to learn languages for each platform.
 2.It can create applications for :
 - Mobile (iOS and Android)
 - Web
 - Desktop (Windows, macOS, Linux)
 3.Flutter uses the Dart programming language, also developed by Google.
+So flutter is a framework and dart is a programming language.
+Where a framework is a collection of pre-written code that developers can use to build applications more efficiently. 
+  NB : Flutter : One Codebase, Multiple Platforms.
+4.You can run Android mobile apps on a Mac using the Android emulator.
