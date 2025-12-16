f9989fa02258bece6d996c163c03d4d6707001cd
Nathaniel Adika <nathanieladikajnr@gmail.com>
2025-12-16 15:56:16 +0000

    notes

--- DartCourse.dart
+++ DartCourse.dart
@@ -10,4 +10,9 @@ Where a framework is a collection of pre-written code that developers can use to
   NB : Flutter : One Codebase, Multiple Platforms.
 4.You can run Android mobile apps on a Mac using the Android emulator.
 5.On macOS, developers can test applications on both Android and iOS platforms because macOS supports the Android emulator as well as Apple’s official iOS simulator through Xcode. 
-In contrast, Windows supports testing on the Android platform using the Android emulator, but it does not support testing on iOS, since Apple restricts iOS simulators and development tools to macOS only
+In contrast, Windows supports testing on the Android platform using the Android emulator, but it does not support testing on iOS, since Apple restricts iOS simulators and development tools to macOS only.
+6.Material Design is a design system created by Google to provide a consistent look and feel across all platforms and devices.
+It includes guidelines for layout, typography, color, and other design elements.
+This can be seen with the MaterialApp widget in Flutter.
+
+Repository for this course: https://github.com/academind/flutter-complete-guide-course-resources
