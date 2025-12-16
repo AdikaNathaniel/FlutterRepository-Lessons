6c3754e7b93d3e14b1082c4931c8fadeb9d198f6
Nathaniel Adika <nathanieladikajnr@gmail.com>
2025-12-16 06:30:24 +0000

    notes

---
+++ DartCourse.dart
@@ -0,0 +1,6 @@
+1.Flutter is a UI framework developed by Google for building natively compiled applications for mobile, web, and desktop from a single codebase.
+2.It can create applications for :
+- Mobile (iOS and Android)
+- Web
+- Desktop (Windows, macOS, Linux)
+3.Flutter uses the Dart programming language, also developed by Google.
