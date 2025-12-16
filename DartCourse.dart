bb94bb6edd1687598266aa27f4f057f68569bfbe
Nathaniel Adika <nathanieladikajnr@gmail.com>
2025-12-16 15:35:04 +0000

    notes

--- DartCourse.dart
+++ DartCourse.dart
@@ -9,3 +9,5 @@ So flutter is a framework and dart is a programming language.
 Where a framework is a collection of pre-written code that developers can use to build applications more efficiently. 
   NB : Flutter : One Codebase, Multiple Platforms.
 4.You can run Android mobile apps on a Mac using the Android emulator.
+5.On macOS, developers can test applications on both Android and iOS platforms because macOS supports the Android emulator as well as Apple’s official iOS simulator through Xcode. 
+In contrast, Windows supports testing on the Android platform using the Android emulator, but it does not support testing on iOS, since Apple restricts iOS simulators and development tools to macOS only
